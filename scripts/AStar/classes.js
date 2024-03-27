2df2779b2c3dea97b0f438b1ff6470bf838198f6
AnastasiaStavrova <158266030+AnastasiaStavrova@users.noreply.github.com>
2024-03-27 20:41:52 +0700

    add a* algorithm

--- scripts/AStar/classes.js
+++ scripts/AStar/classes.js
@@ -1,3 +1,4 @@
+import { fieldPixelsSize } from "./aStar_main.js";
 export class Point {
     constructor(x, y) {
         this.x = x
@@ -6,10 +7,17 @@ export class Point {
 }
 
 export class Cell {
-    constructor(x, y, cellSize) {
-        this.vertex1 = new Point(Math.round(x), Math.round(y));
-        this.vertex2 = new Point(Math.round(x) + Math.round(cellSize), Math.round(y));
-        this.vertex3 = new Point(Math.round(x), Math.round(y) + Math.round(cellSize));
-        this.vertex4 = new Point(Math.round(x) + Math.round(cellSize), Math.round(y) + Math.round(cellSize));
+    constructor(x = null, y = null, cellSize = null, number = null) {
+        this.vertex1 = new Point(x, y);
+        this.vertex2 = new Point(x + cellSize, y);
+        this.vertex3 = new Point(x, y + cellSize);
+        this.vertex4 = new Point(x + cellSize, y + cellSize);
+        this.cellNumber = number;
+        this.posX = (number - 1) % fieldPixelsSize;
+        this.posY = parseInt((number - 1) / fieldPixelsSize);
+        this.parent = null;
+        this.distanceToStart = 0;
+        this.distanceToFinish = 0;
+        this.sumDistances = 0;
     }
 }
