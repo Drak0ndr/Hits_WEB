b1d88884157f56bc92a70d6272fae0007a5d662f
Максим <drakondr.video@gmail.com>
2024-03-28 18:40:35 +0700

    bug fix

--- scripts/ant/draw.js
+++ scripts/ant/draw.js
@@ -17,38 +17,47 @@ export class Draw {
         this.ctx.fill()
         this.vertex.push([x, y])
     }
-    drawLine(x1, y1, x2, y2, color = 'gray') {
+
+    drawLine(x1, y1, x2, y2, color = 'gray', width = 0.4) {
         this.ctx.beginPath()
         this.ctx.strokeStyle = color
         this.ctx.lineJoin = 'miter'
-        this.ctx.lineWidth = 1
+        this.ctx.lineWidth = width
         this.ctx.moveTo(x1, y1)
         this.ctx.lineTo(x2, y2)
         this.ctx.stroke()
     }
+
     drawPaths() {
-        for(let i = 0; i< this.vertex.length; i++) {
-            for(let j = i; j< this.vertex.length; j++) {
+        for (let i = 0; i < this.vertex.length; i++) {
+            for (let j = i; j < this.vertex.length; j++) {
                 this.drawLine(this.vertex[i][0], this.vertex[i][1], this.vertex[j][0], this.vertex[j][1])
             }
         }
     }
+
     drawAns(path) {
-        for(let i = 0; i < path.length-1; i++) {
-            this.drawLine(this.vertex[path[i]][0], this.vertex[path[i]][1], this.vertex[path[i+1]][0], this.vertex[path[i+1]][1], 'green')
+        for (let i = 0; i < path.length - 1; i++) {
+            this.drawLine(this.vertex[path[i]][0], this.vertex[path[i]][1], this.vertex[path[i + 1]][0], this.vertex[path[i + 1]][1], 'green', 1)
         }
     }
+
     buildGraph() {
         let graph = {}
-        for(let i = 0; i<this.vertex.length; i++) {
+        for (let i = 0; i < this.vertex.length; i++) {
             graph[i] = []
-            for(let j = 0; j<this.vertex.length; j++) {
-                let dist = ((this.vertex[i][0]-this.vertex[j][0])**2 + (this.vertex[i][1]-this.vertex[j][1])**2)**0.5
-                if(dist >0) {
-                    graph[i].push([j,dist,0.1])
+            for (let j = 0; j < this.vertex.length; j++) {
+                let dist = ((this.vertex[i][0] - this.vertex[j][0]) ** 2 + (this.vertex[i][1] - this.vertex[j][1]) ** 2) ** 0.5
+                if (dist > 0) {
+                    graph[i].push([j, dist, 0.1])
                 }
             }
         }
         return graph
+    }
+
+    clear() {
+        this.vertex = []
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
     }
 }
--- scripts/ant/main.js
+++ scripts/ant/main.js
@@ -2,6 +2,7 @@ import { Draw } from "./draw.js";
 import { AntAlg } from "./antAlg.js";
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
+const clearBtn = document.querySelector('.clear')
 const startBtn = document.querySelector('.start')
 const ansSpan = document.querySelector('.ans')
 let drawCanvas = new Draw(canvas)
@@ -13,7 +14,11 @@ startBtn.addEventListener('click', (e) => {
     let ant = new AntAlg(graph)
     console.log(ant.getP(0, []))
     console.log(graph)
-    let data = ant.start(20)
+    let data = ant.start(30)
     drawCanvas.drawAns(data[1])
     ansSpan.innerHTML = data[0].toFixed(2)
+})
+
+clearBtn.addEventListener('click', (e) => {
+    drawCanvas.clear()
 })
